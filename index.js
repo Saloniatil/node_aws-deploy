7518093ad29252a62296a81a3d28c3ad2ff27b6a
saloniTech <salonipatil10122001@gmail.com>
2024-08-30 13:51:02 +0530

    new api created

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.json())
 app.get('/api/get', (req, res) => {
   return res.send({message: "CI/CD Integration "})
 })
+app.get('/api/get/user', (req, res) => {
+  return res.send({ name: 'Dev', email: 'saloni@gmail.com', 'role': 'Deveopment' });
+})
 const port = 5000
 
 app.listen(port, () => {
